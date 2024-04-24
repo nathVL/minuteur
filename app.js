7e98431d0d80cc54375bdb1c4ec95d24ae29a026
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-04-24 18:32:13 +0200

    js fini + documentation + ajout readme

--- app.js
+++ app.js
@@ -1,108 +1,159 @@
-// Constantes
+// Constantes et declaration de variable
 const countdown = document.getElementById("countdown");
 const startButton = document.getElementById("startButton");
 const resetButton = document.getElementById("resetButton");
-const plusDizaine = document.getElementById("plusDizaine");
-const plusUnite = document.getElementById("plusUnite");
-const moinsDizaine = document.getElementById("moinsDizaine");
-const moinsUnite = document.getElementById("moinsUnite");
-
+const pauseButton = document.getElementById("pauseButton");
+const plusDizaineMin = document.getElementById("plusDizaineMin");
+const plusUniteMin = document.getElementById("plusUniteMin");
+const moinsDizaineMin = document.getElementById("moinsDizaineMin");
+const moinsUniteMin = document.getElementById("moinsUniteMin");
+const plusDizaineSec = document.getElementById("plusDizaineSec");
+const plusUniteSec = document.getElementById("plusUniteSec");
+const moinsDizaineSec = document.getElementById("moinsDizaineSec");
+const moinsUniteSec = document.getElementById("moinsUniteSec");
 
 let time;
 let timeIni = {
+    "dsec": 0,
     "seconde" : 30,
     "minute" : 0
 };
 let intervalId;
 let estArrete = true;
 
+
+// Fonctions
+/**
+ * Met à jour le compte à rebours en affichant les minutes, les secondes et les dixièmes de seconde.
+ */
 function updateCountDown() {
-    const minutes = Math.floor(time / 600); // Conversion des millisecondes en minutes
-    const seconds = Math.floor((time % 600) / 10); // Récupération des secondes
-    const dsec = time % 10; // Récupération des dixièmes de seconde
-  
-    // Ajout d'un zéro devant les secondes si elles sont inférieures à 10
-    const secondsDisplay = zeroDevant(seconds);
-    const minutesDisplay = zeroDevant(minutes);
-
-    
-  
-    // Affichage des minutes et des secondes
-    countdown.innerHTML = `${minutesDisplay}:${secondsDisplay}:${dsec}`;
-  
+    const minutes = Math.floor(time / 600);
+    const seconds = Math.floor((time % 600) / 10);
+    const dsec = time % 10;
+    afficherTemps(minutes, seconds, dsec);
     if (time <= 0) {
-      clearInterval(intervalId);
+        clearInterval(intervalId);
     } else {
-      time--;
+        time--;
     }
-  }
-  
+} 
 
-function StartTimer(timeSeconds) {
+/**
+ * Démarre le timer avec les secondes spécifiées (et les minutes optionnelles).
+ * @param {number} timeSeconds - Les secondes pour démarrer le timer.
+ * @param {number} [timeMinutes=0] - Les minutes optionnelles pour démarrer le timer.
+ */
+function StartTimer(timeSeconds, timeMinutes = 0) {
     estArrete = false;
-    time = timeSeconds.seconde * 10;
+    time = (timeMinutes * 60 + timeSeconds) * 10;
     intervalId = setInterval(updateCountDown, 100);
 }
 
+/**
+ * Réinitialise le timer et affiche le temps initial.
+ */
 function resetTimer() {
     estArrete = false;
     clearInterval(intervalId);
     afficherTemps(timeIni.minute, timeIni.seconde);
 }
 
+/**
+ * Arrête ou redémarre le timer en fonction de son état actuel.
+ */
+function stopTimer() {
+    if (!estArrete) {
+        clearInterval(intervalId);
+        estArrete = true;
+    } else {
+        StartTimer(time/10);
+        estArrete = false;
+    }
+}
 
+/**
+ * Ajoute un zéro devant un nombre si celui-ci est inférieur à 10.
+ * @param {number} val - La valeur à formater.
+ * @returns {string} La valeur formatée avec un zéro devant si nécessaire.
+ */
 function zeroDevant(val) {
     let newVal = val;
     newVal = newVal < 10 ? '0' + newVal : newVal;
     return newVal
 }
 
-function afficherTemps(minute, seconde) {
+/**
+ * Affiche le temps dans le format 'mm:ss:dsec'.
+ * @param {number} [minute=0] - Les minutes à afficher.
+ * @param {number} [seconde=0] - Les secondes à afficher.
+ * @param {number} [dsec=0] - Les dixièmes de seconde à afficher.
+ */
+function afficherTemps(minute=0, seconde=0, dsec=0) {
     let minAfficher = zeroDevant(minute);
     let secAfficher = zeroDevant(seconde);
-    countdown.innerHTML = `${minAfficher}:${secAfficher}:0`;
+    countdown.innerHTML = `${minAfficher}:${secAfficher}:${dsec}`;
 }
 
-// Boutons
-// Ajout temps
-plusDizaine.addEventListener("click", function() {
-    timeIni.seconde += 10;
-    afficherTemps(timeIni.minute, timeIni.seconde);
-});
-
-plusUnite.addEventListener("click", function() {
-    timeIni.seconde += 1;
-    afficherTemps(timeIni.minute, timeIni.seconde);
-});
-// Supp temps
-moinsDizaine.addEventListener("click", function() {
-    if (timeIni.seconde - 10 >= 0) {
-        timeIni.seconde -= 10;
-        afficherTemps(timeIni.minute, timeIni.seconde);
+/**
+ * Vérifie et ajuste les eurs de secondes pour qu'elles restent dans les plages valides.
+ */
+function passerMinutes() {
+    if (timeIni.seconde >= 60) {
+        timeIni.seconde -= 60;
+        timeIni.minute++;   
     }
-});
+}
 
-moinsUnite.addEventListener("click", function() {
-    if (timeIni.seconde - 1 >= 0) {
-        timeIni.seconde -= 1;
-        afficherTemps(timeIni.minute, timeIni.seconde);
+/**
+ * Retire un certain nombre de secondes et de minutes du temps initial et affiche le temps mis à jour.
+ * @param {number} sec - Le nombre de secondes à retirer.
+ * @param {number} [minute=0] - Le nombre de minutes à retirer.
+ */
+function retirerTemps(sec, minute = 0) {
+    if (timeIni.minute - minute >= 0) {
+        timeIni.minute -= minute;
     }
-});
+    if (sec != 0) {
+        if (timeIni.minute > 0) {
+            if (timeIni.seconde - sec >= 0) {
+                timeIni.seconde -= sec
+            } else {
+                timeIni.minute--;
+                timeIni.seconde = 60 - sec + timeIni.seconde;
+            }
+        } else {
+            if (timeIni.seconde - sec >= 0) {
+                timeIni.seconde -= sec;
+        }}}
+    afficherTemps(timeIni.minute, timeIni.seconde);
+}
 
+/**
+ * Ajoute un certain nombre de secondes et de minutes au temps initial et affiche le temps mis à jour.
+ * @param {number} sec - Le nombre de secondes à ajouter.
+ * @param {number} [minute=0] - Le nombre de minutes à ajouter.
+ */
+function ajouterTemps(sec, minute = 0) {
+    timeIni.seconde += sec;
+    timeIni.minute += minute;
+    passerMinutes();
+    afficherTemps(timeIni.minute, timeIni.seconde);
+}
 
-// Controle boutons
+// Controles des boutons
+plusDizaineSec.addEventListener("click", () => ajouterTemps(10));
+plusUniteSec.addEventListener("click",  () => ajouterTemps(1));
+plusDizaineMin.addEventListener("click",  () => ajouterTemps(0, 10));
+plusUniteMin.addEventListener("click",  () => ajouterTemps(0, 1));
+
+moinsDizaineSec.addEventListener("click", () => retirerTemps(10));
+moinsUniteSec.addEventListener("click", () => retirerTemps(1));
+moinsDizaineMin.addEventListener("click", () => retirerTemps(0,10));
+moinsUniteMin.addEventListener("click", () => retirerTemps(0,1));
+
 startButton.addEventListener("click", function() {
-    let count = timeIni;
-    StartTimer(count);
+    resetTimer();
+    StartTimer(timeIni.seconde, timeIni.minute);
 });
-
 resetButton.addEventListener("click",resetTimer);
-
-pauseButton.addEventListener("click", function() {
-    if (!estArrete) {
-        clearInterval(intervalId);
-    } else {
-        StartTimer(seconds);
-        estArrete = false;
-    }
-});
+pauseButton.addEventListener("click", stopTimer)
